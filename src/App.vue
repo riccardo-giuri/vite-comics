fcfbf486a9ebfbac0c324fbe0901fa8a3ea35b0a
riccardo-giuri <richicryst@gmail.com>
2023-08-28 17:57:00 +0200

    comic cards added with jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
 import TheBlueBanner from "./components/TheBlueIconBanner.vue"
+import comicsData from "./db/comics_data"
+import comicCard from "./components/ComicCard.vue"
 
 export default {
   components: {
     TheHeader,
     TheFooter,
-    TheBlueBanner
+    TheBlueBanner,
+    comicCard
+  },
+
+  data() {
+    return {
+      comicsData
+    }
   }
 }
 </script>
@@ -15,13 +24,41 @@ export default {
 <template>
   <TheHeader></TheHeader>
 
-  <main></main>
+  <main>
+    <div class="jumbotron"></div>
+
+    <div class="comicsContainer">
+      <div class="container">
+        <div class="row row-cols-6">
+          <div
+            class="col" 
+            v-for="(comicData, i) in comicsData"
+            :key="`comic_${i}`"
+          >
+            <comicCard :singleComicData="comicData"></comicCard>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </main>
 
   <TheBlueBanner></TheBlueBanner>
 
   <TheFooter></TheFooter>
 </template>
 
-<style scoped>
+<style lang="scss">
+  @use "./styles/partials/variables" as *;
 
+  .comicsContainer {
+    background-color: $color_Dark;
+    padding: 3rem 0rem;
+  }
+
+  .jumbotron {
+    min-height: 370px;
+    background-image: url("./assets/jumbotron.jpg");
+    background-position-y: 5%;
+  }
 </style>
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        props: {
+            singleComicData: Object,
+        }
+    }
+</script>
+
+<template>
+    <div class="cardContainer">
+        <img :src="singleComicData.thumb" alt="">
+
+        <div class="comicName"> {{ singleComicData.series }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/variables" as *;
+
+    img {
+        width: 100%;
+        height: 255px;
+    }
+
+    .comicName {
+        text-transform: uppercase;
+        color: white;
+        font-weight: bold;
+        font-size: 0.75rem;
+    }
+
+    .cardContainer {
+        margin-bottom: 0.8rem;
+    }
+</style>
